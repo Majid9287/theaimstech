9207a7dae46f507bf3665b0abcaf87289b0379b1
Majid <abdulmajid9287@gmail.com>
2023-05-12 20:47:55 +0500

    update login

--- pages/_app.js
+++ pages/_app.js
@@ -65,7 +65,7 @@ export default function App({ Component, pageProps }) {
     } else {
       setIsUserLoggedIn(false);
     }
-  }, [isUserLoggedIn, token]);
+  }, [token]);
   const handleSignIn = (isLoggedIn) => {
     setIsUserLoggedIn(isLoggedIn);
   };
